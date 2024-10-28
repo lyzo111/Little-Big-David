d2effa02ae4008ec52b0efe8e8b1177c67eb04bb
Lyzo <chrisus999@gmail.com>
2024-10-28 06:53:14 +0100

    renamed tables, changed types integer to int, added charTask table

--- Database.py
+++ Database.py
@@ -7,30 +7,39 @@ connection = sqlite3.connect('littleBigDatabase.db')
 cursor = connection.cursor()
 
 # Create tables if non-existing
-cursor.execute('''CREATE TABLE IF NOT EXISTS chars (
-                    charID INTEGER PRIMARY KEY AUTOINCREMENT,
+cursor.execute('''CREATE TABLE IF NOT EXISTS char (
+                    charID INT PRIMARY KEY AUTOINCREMENT ON DELETE CASCADE,
                     name TEXT NOT NULL,
                     race TEXT NOT NULL,
-                    roll TEXT NOT NULL,
+                    roll TEXT NOT NULL
 )''')
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS charStats (
-                    charID INTEGER FOREIGN KEY (charID) REFERENCES chars (charID) ON DELETE CASCADE,
-                    level INTEGER NOT NULL DEFAULT 1,
-                    charisma INTEGER  NOT NULL DEFAULT 1,
-                    crafting INTEGER  NOT NULL DEFAULT 1,
-                    health INTEGER  NOT NULL DEFAULT 1,
-                    strength INTEGER  NOT NULL DEFAULT 1,
-                    defense INTEGER  NOT NULL DEFAULT 1,
-                    intelligence INTEGER  NOT NULL DEFAULT 1,
-                    luck INTEGER  NOT NULL DEFAULT 1,
+cursor.execute('''CREATE TABLE IF NOT EXISTS statTable (
+                    charID INT FOREIGN KEY (charID) ON DELETE CASCADE,
+                    level INT NOT NULL DEFAULT 10,
+                    charisma INT NOT NULL DEFAULT 10,
+                    crafting INT NOT NULL DEFAULT 10,
+                    health INT NOT NULL DEFAULT 10,
+                    strength INT NOT NULL DEFAULT 10,
+                    defense INT NOT NULL DEFAULT 10,
+                    intelligence INT NOT NULL DEFAULT 10,
+                    luck INT NOT NULL DEFAULT 10,
+                    CONSTRAINT fk_charID FOREIGN KEY (charID) REFERENCES char(charID)
 )''')
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS tasks (
-                    taskID INTEGER PRIMARY KEY AUTOINCREMENT,
+cursor.execute('''CREATE TABLE IF NOT EXISTS task (
+                    taskID INT PRIMARY KEY AUTOINCREMENT,
                     description TEXT NOT NULL,
-                    XP INTEGER NOT NULL,
-                    date DATE NOT NULL,
+                    XP INT NOT NULL,
+                    date DATE NOT NULL
+)''')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS charTask (
+                    charID INT,
+                    taskID INT,
+                    PRIMARY KEY (charID, taskID),
+                    CONSTRAINT fk_char FOREIGN KEY (charID) REFERENCES char(charID) ON DELETE CASCADE,
+                    CONSTRAINT fk_task FOREIGN KEY (taskID) REFERENCES task(taskID) ON DELTETE CASCADE
 )''')
 
 # Save changes and close connection
