3e783a19862977fa24cbd6ca60d71be062db1379
Lyzo <chrisus999@gmail.com>
2024-11-19 15:08:17 +0100

    inverted bool that checks if user exists to make more sense with method naming. please re-check logic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # This is a Little Big David.
 import sqlite3
 
+def invert_bool(my_bool: bool) -> bool:
+    return not my_bool
+
 def first_time() -> bool:
     conn = sqlite3.connect('littleBigDatabase.db')
     cursor = conn.cursor()
@@ -9,7 +12,10 @@ def first_time() -> bool:
         cursor.execute("SELECT EXISTS(SELECT 1 FROM char)")
         result = bool(cursor.fetchone()[0])
     finally:
-        return result if result is not None else False
+        invert_bool(result if result is not None else False)
 
 if __name__ == '__main__':
-    first_time()
+    if first_time():
+        # Add character creation
+        # Add tutorial (skippable)
+        print()
