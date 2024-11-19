4d1f67c670f9172be89bf7c3774393808ebef644
Lyzo <chrisus999@gmail.com>
2024-11-19 14:49:16 +0100

    checking if this is first time playing, removed sample method

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # This is a Little Big David.
+import sqlite3
 
-
-def print_hi(name):
-    print(f'Hi, {name}')
-
+def first_time() -> bool:
+    conn = sqlite3.connect('littleBigDatabase.db')
+    cursor = conn.cursor()
+    result = None
+    try:
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM char)")
+        result = bool(cursor.fetchone()[0])
+    finally:
+        return result if result is not None else False
 
 if __name__ == '__main__':
-    print_hi('David')
+    first_time()
