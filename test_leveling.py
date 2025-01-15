b66b097f3d9df2705d5700a2df154bbfce7dcec7
alexander.schleh <alexander.schleh@burkert.com>
2025-01-15 09:32:55 +0100

    Added test for leveling

---
+++ test_leveling.py
@@ -0,0 +1,18 @@
+from character_options import CharTaskOperations
+from character import Character
+from task_operations import TaskOperations
+
+if __name__ == "__main__":
+    char_ops = Character()
+    task_ops = TaskOperations()
+    char_task_ops = CharTaskOperations()
+
+    # Charakter und Aufgabe erstellen
+    char_id = char_ops.create_character("Thrall", "Orc", "Shaman")
+    task_id = task_ops.create_task("Save the Horde", 120, "2025-03-01")
+
+    # Aufgabe dem Charakter zuweisen
+    char_task_ops.assign_task_to_character(char_id, task_id)
+
+    # Aufgabe als abgeschlossen markieren
+    char_task_ops.mark_task_as_completed(char_id, task_id)
