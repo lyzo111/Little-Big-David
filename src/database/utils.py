ba8ca730ffa9b6d9c6482036dc16f0709372bf68
christoph.martin <christoph.martin@burkert.com>
2025-01-20 18:46:43 +0100

    added method for getting classes and races

---
+++ src/database/utils.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+from src.database.database import Database
+
+db = Database()
+
+def get_classes_and_races():
+    connection = db.create_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT name FROM classname")
+    classes = [row[0] for row in cursor.fetchall()]
+
+    cursor.execute("SELECT name FROM race")
+    races = [row[0] for row in cursor.fetchall()]
+
+    connection.close()
+    return classes, races
+
