218fb4e3da49ae814c2ea7928ffb24f341d19032
christoph.martin <christoph.martin@burkert.com>
2025-02-17 13:58:24 +0100

    fix getting tasks, make code more modular, adjust comments

--- src/database/utils.py
+++ src/database/utils.py
@@ -2,7 +2,7 @@ from src.database.database import db
 
 default_pfp = "assets/default_pfp.jpg"
 
-def get_data(table_name):
+def get_data(attribute, table):
     """
     Retrieves data from a specified table in the database.
 
@@ -12,11 +12,11 @@ def get_data(table_name):
     Returns:
         list: A list of names retrieved from the specified table.
     """
-    query = f"SELECT name FROM {table_name}"
+    query = f"SELECT {attribute} FROM {table}"
     connection = db.create_connection()
     cursor = connection.cursor()
     cursor.execute(query)
-    data = [row[0] for row in cursor.fetchall()]
+    data = cursor.fetchall()
     connection.close()
     return data
 
@@ -24,25 +24,48 @@ def get_classes():
     """
     Retrieves class names from the 'classname' table in the database.
 
+    Args:
+        attribute (str): The attribute to retrieve data from.
+        table (str): The name of the table to retrieve data from.
+
     Returns:
         list: A list of class names.
     """
-    return get_data("classname")
+    attribute = "name"
+    table = "classname"
+    return get_data(attribute, table)
 
 def get_races():
     """
     Retrieves race names from the 'race' table in the database.
 
+    Args:
+        attribute (str): The attribute to retrieve data from.
+        table (str): The name of the table to retrieve data from.
+
     Returns:
         list: A list of race names.
     """
-    return get_data("race")
+    attribute = "name"
+    table = "race"
+    return get_data(attribute, table)
 
 def get_tasks():
     """
     Retrieves task descriptions from the 'task' table in the database.
 
+    Args:
+        attribute (str): All attributes of table.
+            [0] -> ID
+            [1] -> Description
+            [2] -> XP
+            [3] -> Expiration Date
+        table (str): The name of the table to retrieve data from.
+
     Returns:
         list: A list of task descriptions.
     """
-    return get_data("task")
+
+    attribute = "*"
+    table = "task"
+    return get_data(attribute, table)
